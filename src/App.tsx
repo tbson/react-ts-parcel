61347edb4e65415bd3c74b50ad07d42578d91e31
Son Tran <tbson87@gmail.com>
2020-01-17 15:28:20 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+const Main = () => {
+    return <h1>Hello World</h1>;
+};
+export default Main;
---
+++ src/Index.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+
+// @ts-ignore
+if (module.hot) module.hot.accept();
+ReactDOM.render(React.createElement(App), document.getElementById('app'));
